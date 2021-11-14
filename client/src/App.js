f0e737af19bb5a06d4488571af4f35b11770becd
Richard <32272387+lauyh@users.noreply.github.com>
2021-11-15 01:09:07 +0800

    add auth and private route

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,40 @@ import NavBar from "./Components/Layouts/NavBar"
 import Home from "./Components/Pages/Home";
 import About from "./Components/Pages/About";
 import ContactState from "./Components/Contexts/Contact/ContactState";
+import AuthState from './Components/Contexts/Auth/AuthState';
+import AlertState from './Components/Contexts/Alert/AlertState';
+import { Register } from './Components/Auth/Register';
+import {Login} from './Components/Auth/Login'
+import { Alerts } from './Components/Layouts/Alerts';
+import setAuthToken from './Components/Utils/setAuthToken';
+import { PrivateRoute } from './Components/Routing/PrivateRoute';;
 
+if(localStorage.token){
+  setAuthToken(localStorage.token)
+}
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar/> 
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route exact path="/about" component={About}/>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <NavBar/> 
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <PrivateRoute exact path="/" component={Home}/>
+                  <Route exact path="/about" component={About}/>
+                  <Route exact path="/register" component={Register}/>
+                  <Route exact path="/login" component={Login}/>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 }
 
