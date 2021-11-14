373a52f964852533012b4bbd2349c0443c20aae9
Richard <32272387+lauyh@users.noreply.github.com>
2021-11-15 01:09:48 +0800

    add auth state

--- client/src/Components/Pages/Home.jsx
+++ client/src/Components/Pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import Contacts from "../Contacts/Contacts";
 import ContactForm from '../Contacts/ContactForm';
 import { ContactFilter } from '../Contexts/Contact/ContactFilter';
+import AuthContext from '../Contexts/Auth/AuthContext';
+
 
 const Home = () => {
+	const authContext = useContext(AuthContext);
+	useEffect(()=>{
+		// console.log("home page load")
+		authContext.loadUser();
+		// eslint-disable-next-line
+	}, [])
 	return (
 		<div className="grid-2">
 			<div>
